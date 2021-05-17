42fdd2853f219ffb96806c80c28141e9b2571afa
Maulia Farsiko Gama <farsikogama@gmail.com>
2021-05-17 08:10:51 +0700

    adding responsive layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,33 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import { device } from '../components/MediaQueries'
 
 // import components
-import Link from 'next/link'
 import Image from 'next/image'
 
 // styling
 
 const CardDiv = styled.div`
   background: #1c1f27;
-  flex-basis: 30%;
-  margin: 1rem;
+  margin: 0.75rem;
   border: 1px solid black;
-  border-radius: 3px;
+  border-radius: 20px;
   box-shadow: 1px 1px 0px black;
-  padding: 30px;
   cursor: pointer;
   &:hover {
     background: #282931;
+  }
+  @media ${device.mobileS} {
+    height: 35vh;
+    flex-basis: 70%;
+  }
+  @media ${device.mobileM} {
+  }
+  @media ${device.mobileL} {
+  }
+  @media ${device.tablet} {
+    flex-basis: 30%;
+  }
+  @media ${device.laptop} {
   }
 `
 
@@ -24,7 +35,7 @@ const Card = props => {
   return (
     <>
       <CardDiv>
-        <Image src='/img/farsiko.jpg' height='225' width='auto' />
+        {/* <Image src='/img/farsiko.jpg' height='225' width='auto' /> */}
         <p>tes image</p>
       </CardDiv>
     </>
